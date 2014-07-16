efe911a3dcd5fac3439f17d56c42b250dd59fa9f
Alberto Illobre <aldoilsant@gmail.com>
2014-07-16 13:54:33 +0200

    Added complete test main method

--- src/org/sigaim/siie/clients/TestMain.java
+++ src/org/sigaim/siie/clients/TestMain.java
@@ -1,6 +1,8 @@
 package org.sigaim.siie.clients;
 
+import org.sigaim.siie.clients.ws.WSIntSIIE001EQLClient;
 import org.sigaim.siie.clients.ws.WSIntSIIE004ReportManagementClient;
+import org.sigaim.siie.dadl.OpenEHRDADLManager;
 import org.sigaim.siie.iso13606.rm.CDCV;
 import org.sigaim.siie.iso13606.rm.Composition;
 import org.sigaim.siie.iso13606.rm.EHRExtract;
@@ -8,16 +10,23 @@ import org.sigaim.siie.iso13606.rm.FunctionalRole;
 import org.sigaim.siie.iso13606.rm.HealthcareFacility;
 import org.sigaim.siie.iso13606.rm.II;
 import org.sigaim.siie.iso13606.rm.Performer;
+import org.sigaim.siie.seql.model.SEQLResultSet;
 
 public class TestMain {
 	
 	public static void main(String[] args) {
 		WSIntSIIE004ReportManagementClient client;
+		WSIntSIIE001EQLClient eqlClient;
+
+		OpenEHRDADLManager manager=new OpenEHRDADLManager();
+
 		if(args.length==1) {
 			//http://localhost:8080/SIIEWS/services/INTSIIE001EQLImpl
 			client=new WSIntSIIE004ReportManagementClient(args[0]);
+			eqlClient=new WSIntSIIE001EQLClient(args[0]);
 		} else if(args.length==0) {
 			client=new WSIntSIIE004ReportManagementClient();
+			eqlClient=new WSIntSIIE001EQLClient();
 		} else {
 			System.err.println("Expected 0 parameters (connection to localhost:8080) or 1 parameter (endpoint url, like http://localhost:8080/SIIEWS/services/INTSIIE004ReportManagementImpl)");
 			return;
@@ -38,6 +47,19 @@ public class TestMain {
 			reportStatus.setCode("RSTA02");
 			Composition newReport=client.createReport("4", newEHR.getEhrId(), composer, "", "some text", reportStatus, rootArchetypeId);
 			System.out.println("New report id: "+newReport.getRcId().getRoot()+" "+newReport.getRcId().getExtension());
+			SEQLResultSet rs=eqlClient.query("1", "SELECT c FROM EHR CONTAINS COMPOSITION c WHERE c/rc_id/extension="+newReport.getRcId().getExtension()+";");
+			int nrow=0;
+			while(rs.nextRow()) {
+				int i;
+				for(i=0;i<rs.getNumberOfColumns();i++) {
+					System.out.println("Row: "+nrow+" Column: "+i+": "+manager.serialize(rs.getColumn(i),false));
+				}
+				nrow++;
+			} 
+			if(nrow==0) {
+				System.out.println("No results found...");
+			}
+			System.out.println("ALL DONE!");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
